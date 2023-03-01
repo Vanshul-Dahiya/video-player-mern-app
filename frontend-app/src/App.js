e42edc465f4536515fcff5d3319ba7eeaac86f16
Vanshul <vanshuldahiya2002@gmail.com>
2023-03-01 18:47:35 +0530

    single video view page done

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,14 +1,48 @@
+import styled from "styled-components";
 import Videos from "./Components/Videos";
 import { useGlobalContext } from "./context/global";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VideoPlayer from "./Components/VideoPlayer";
 
 function App() {
   const global = useGlobalContext();
   console.log(global);
   return (
-    <div className="App">
-      <Videos />
-    </div>
+    <BrowserRouter>
+      <AppStyled className="App">
+        <h1>Video Uploader</h1>
+        <Routes>
+          <Route path="/" element={<Videos />} />
+          <Route path="/videos/:id" element={<VideoPlayer />} />
+        </Routes>
+        {/* <Videos /> */}
+      </AppStyled>
+    </BrowserRouter>
   );
 }
-
+const AppStyled = styled.div`
+  padding: 3rem 18rem;
+  h1 {
+    color: #fff;
+    background: linear-gradient(to right, #00b894 40%, #705df2);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    display: inline-block;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.5);
+  }
+  .upload {
+    display: flex;
+    justify-content: flex-start;
+  }
+`;
 export default App;
--- frontend-app/src/context/global.js
+++ frontend-app/src/context/global.js
@@ -1,11 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const GlobalContext = React.createContext();
 
+// actions
+const LOADING = "LOADING";
+const SET_VIDEOS = "SET_VIDEOS";
+const SET_SELECTED_VIDEO = "SET_SELECTED_VIDEO";
+
+// access payload in reducer to change state accordingly
+const reducer = (state, action) => {
+  switch (action.type) {
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SET_VIDEOS:
+      return {
+        ...state,
+        loading: false,
+        // change video state
+        videos: [
+          ...action.payload.map((video) => {
+            return {
+              ...video,
+              videoUrl: `http://localhost:3000/public/videos/${video.filename}`,
+            };
+          }),
+        ],
+      };
+    default:
+      return state;
+  }
+  return state;
+};
+
 // it will contain whole app
 export const GlobalProvider = ({ children }) => {
+  const initialState = {
+    videos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  // get videos
+  const getAllVideos = async () => {
+    try {
+      // fetch all videos
+      const res = await fetch("http://localhost:3000/api/videos");
+      const data = await res.json();
+
+      // dispatch an data to payload
+      dispatch({ type: SET_VIDEOS, payload: data.videos });
+
+      console.log(data.videos);
+    } catch (e) {}
+  };
+
+  // on initial render ,fetch the videos
+  useEffect(() => {
+    getAllVideos();
+  }, []);
   return (
-    <GlobalContext.Provider value={"Hello"}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider value={{ ...state }}>
+      {children}
+    </GlobalContext.Provider>
   );
 };
 
